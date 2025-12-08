c107da7d197906c23c4e7c62a9b07c3efcfe1a90
FerdiansyahDwi <ferdiansyahdwi00@gmail.com>
2025-12-08 10:22:34 +0700

    Get Catatan

--- app/src/main/java/com/example/latihanapi/repository/CatatanRepository.kt
+++ app/src/main/java/com/example/latihanapi/repository/CatatanRepository.kt
@@ -3,10 +3,14 @@ package com.example.latihanapi.repository
 import com.example.latihanapi.entities.Catatan
 import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
 
 
 interface CatatanRepository {
     @POST("catatan")
     suspend fun createCatatan(@Body catatan: Catatan): Response<Catatan>
+
+    @GET("catatan")
+    suspend fun getCatatan(): Response<List<Catatan>>
 }
